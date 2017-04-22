6c6fceea929db68050d75be7a6fb7ecadc31cf1c
julio <jctsiqueira@gmail.com>
2017-04-22 11:18:20 -0300

    creating methods to calculate salary

--- app/src/main/java/com/cesarsicas/salary/Domains/Salary/Activities/MainActivity.kt
+++ app/src/main/java/com/cesarsicas/salary/Domains/Salary/Activities/MainActivity.kt
@@ -2,12 +2,45 @@ package com.cesarsicas.salary.Domains.Salary.Activities
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import com.cesarsicas.salary.R
 
 class MainActivity : AppCompatActivity() {
+    var salary:Double?=null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val calculateSalaryButton = findViewById(R.id.calculateButton) as Button
+        val salaryEditText = findViewById(R.id.salaryEditText) as EditText
+
+        calculateSalaryButton.setOnClickListener {
+            this.salary = salaryEditText.text.toString().toDouble()
+
+        }
+
     }
+
+    public fun calculateSalary(){
+
+    }
+
+    //IR
+    public fun calculateIncomeTax(){
+
+    }
+
+    //INSS
+    public fun calculateINSS(){
+
+    }
+
+    //other discounts
+    public fun calculateDiscounts(){
+
+    }
+
+
 }
---
+++ app/src/test/java/com/cesarsicas/salary/SalaryUnitTest.java
@@ -0,0 +1,8 @@
+package com.cesarsicas.salary;
+
+/**
+ * Created by julio on 22/04/17.
+ */
+
+public class SalaryUnitTestmina {
+}
